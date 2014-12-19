f6f12de4efb2a3930b5510da16bf4c81a2eb7acd
Administrator <Administrator@A8KF5UX3HDOWCSQ>
2014-12-19 16:38:21 +0800

    yhm121902

---
+++ src/com/mingda/dto/TempJzDTO.java
@@ -0,0 +1,472 @@
+package com.mingda.dto;
+
+
+public class TempJzDTO {
+
+	private String tjzId;
+	private String familyid;
+	private String familyno;
+	private String masterid;
+	private String mastername;
+	private String paperid;
+	private String percount;
+	private String salcount;
+	private String operstate;
+	private String on_no;
+	private String famsort;
+	private String accounts;
+	private String onallname;
+	private String fde;
+	private String fdefamilyno;
+	private String fdename;
+	private String f_income;
+	private String fm_sex;
+	private String fmage;
+	private String f_familyid;
+	private String xm_jtcy0;
+	private String sfzh_jtcy0;
+	private String rel0;
+	private String res0;
+	private String body0;
+	private String xm_jtcy1;
+	private String sfzh_jtcy1;
+	private String rel1;
+	private String res1;
+	private String body1;
+	private String xm_jtcy2;
+	private String sfzh_jtcy2;
+	private String rel2;
+	private String res2;
+	private String body2;
+	private String xm_jtcy3;
+	private String sfzh_jtcy3;
+	private String rel3;
+	private String res3;
+	private String body3;
+	private String xm_jtcy4;
+	private String sfzh_jtcy4;
+	private String rel4;
+	private String res4;
+	private String body4;
+	private String qx;
+	private String jd;
+	private String sq;
+	private String approveresult1;
+	private String approveperson1;
+	private String approveidea1;
+	private String aprrovedate1;
+	private String approveresult2;
+	private String approveperson2;
+	private String approveidea2;
+	private String aprrovedate2;
+	private String approveresult3;
+	private String approveperson3;
+	private String approveidea3;
+	private String aprrovedate3;
+	private String approvegoto;
+	private String serialno;
+	private String createtime;
+	private String approveend;
+	private String updatetime;
+	private String approvemoney;
+	public String getFamilyid() {
+		return familyid;
+	}
+	public void setFamilyid(String familyid) {
+		this.familyid = familyid;
+	}
+	public String getFamilyno() {
+		return familyno;
+	}
+	public void setFamilyno(String familyno) {
+		this.familyno = familyno;
+	}
+	public String getMasterid() {
+		return masterid;
+	}
+	public void setMasterid(String masterid) {
+		this.masterid = masterid;
+	}
+	public String getMastername() {
+		return mastername;
+	}
+	public void setMastername(String mastername) {
+		this.mastername = mastername;
+	}
+	public String getPaperid() {
+		return paperid;
+	}
+	public void setPaperid(String paperid) {
+		this.paperid = paperid;
+	}
+	public String getPercount() {
+		return percount;
+	}
+	public void setPercount(String percount) {
+		this.percount = percount;
+	}
+	public String getSalcount() {
+		return salcount;
+	}
+	public void setSalcount(String salcount) {
+		this.salcount = salcount;
+	}
+	public String getOperstate() {
+		return operstate;
+	}
+	public void setOperstate(String operstate) {
+		this.operstate = operstate;
+	}
+	public String getOn_no() {
+		return on_no;
+	}
+	public void setOn_no(String on_no) {
+		this.on_no = on_no;
+	}
+	public String getFamsort() {
+		return famsort;
+	}
+	public void setFamsort(String famsort) {
+		this.famsort = famsort;
+	}
+	public String getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(String accounts) {
+		this.accounts = accounts;
+	}
+	public String getOnallname() {
+		return onallname;
+	}
+	public void setOnallname(String onallname) {
+		this.onallname = onallname;
+	}
+	public String getFde() {
+		return fde;
+	}
+	public void setFde(String fde) {
+		this.fde = fde;
+	}
+	public String getFdefamilyno() {
+		return fdefamilyno;
+	}
+	public void setFdefamilyno(String fdefamilyno) {
+		this.fdefamilyno = fdefamilyno;
+	}
+	public String getFdename() {
+		return fdename;
+	}
+	public void setFdename(String fdename) {
+		this.fdename = fdename;
+	}
+
+	public String getF_income() {
+		return f_income;
+	}
+	public void setF_income(String f_income) {
+		this.f_income = f_income;
+	}
+	public String getFm_sex() {
+		return fm_sex;
+	}
+	public void setFm_sex(String fm_sex) {
+		this.fm_sex = fm_sex;
+	}
+	public String getFmage() {
+		return fmage;
+	}
+	public void setFmage(String fmage) {
+		this.fmage = fmage;
+	}
+	public String getF_familyid() {
+		return f_familyid;
+	}
+	public void setF_familyid(String f_familyid) {
+		this.f_familyid = f_familyid;
+	}
+	public String getXm_jtcy0() {
+		return xm_jtcy0;
+	}
+	public void setXm_jtcy0(String xm_jtcy0) {
+		this.xm_jtcy0 = xm_jtcy0;
+	}
+	public String getSfzh_jtcy0() {
+		return sfzh_jtcy0;
+	}
+	public void setSfzh_jtcy0(String sfzh_jtcy0) {
+		this.sfzh_jtcy0 = sfzh_jtcy0;
+	}
+	public String getRel0() {
+		return rel0;
+	}
+	public void setRel0(String rel0) {
+		this.rel0 = rel0;
+	}
+	public String getRes0() {
+		return res0;
+	}
+	public void setRes0(String res0) {
+		this.res0 = res0;
+	}
+	public String getBody0() {
+		return body0;
+	}
+	public void setBody0(String body0) {
+		this.body0 = body0;
+	}
+	public String getXm_jtcy1() {
+		return xm_jtcy1;
+	}
+	public void setXm_jtcy1(String xm_jtcy1) {
+		this.xm_jtcy1 = xm_jtcy1;
+	}
+	public String getSfzh_jtcy1() {
+		return sfzh_jtcy1;
+	}
+	public void setSfzh_jtcy1(String sfzh_jtcy1) {
+		this.sfzh_jtcy1 = sfzh_jtcy1;
+	}
+	public String getRel1() {
+		return rel1;
+	}
+	public void setRel1(String rel1) {
+		this.rel1 = rel1;
+	}
+	public String getRes1() {
+		return res1;
+	}
+	public void setRes1(String res1) {
+		this.res1 = res1;
+	}
+	public String getBody1() {
+		return body1;
+	}
+	public void setBody1(String body1) {
+		this.body1 = body1;
+	}
+	public String getXm_jtcy2() {
+		return xm_jtcy2;
+	}
+	public void setXm_jtcy2(String xm_jtcy2) {
+		this.xm_jtcy2 = xm_jtcy2;
+	}
+	public String getSfzh_jtcy2() {
+		return sfzh_jtcy2;
+	}
+	public void setSfzh_jtcy2(String sfzh_jtcy2) {
+		this.sfzh_jtcy2 = sfzh_jtcy2;
+	}
+	public String getRel2() {
+		return rel2;
+	}
+	public void setRel2(String rel2) {
+		this.rel2 = rel2;
+	}
+	public String getRes2() {
+		return res2;
+	}
+	public void setRes2(String res2) {
+		this.res2 = res2;
+	}
+	public String getBody2() {
+		return body2;
+	}
+	public void setBody2(String body2) {
+		this.body2 = body2;
+	}
+	public String getXm_jtcy3() {
+		return xm_jtcy3;
+	}
+	public void setXm_jtcy3(String xm_jtcy3) {
+		this.xm_jtcy3 = xm_jtcy3;
+	}
+	public String getSfzh_jtcy3() {
+		return sfzh_jtcy3;
+	}
+	public void setSfzh_jtcy3(String sfzh_jtcy3) {
+		this.sfzh_jtcy3 = sfzh_jtcy3;
+	}
+	public String getRel3() {
+		return rel3;
+	}
+	public void setRel3(String rel3) {
+		this.rel3 = rel3;
+	}
+	public String getRes3() {
+		return res3;
+	}
+	public void setRes3(String res3) {
+		this.res3 = res3;
+	}
+	public String getBody3() {
+		return body3;
+	}
+	public void setBody3(String body3) {
+		this.body3 = body3;
+	}
+	public String getXm_jtcy4() {
+		return xm_jtcy4;
+	}
+	public void setXm_jtcy4(String xm_jtcy4) {
+		this.xm_jtcy4 = xm_jtcy4;
+	}
+	public String getSfzh_jtcy4() {
+		return sfzh_jtcy4;
+	}
+	public void setSfzh_jtcy4(String sfzh_jtcy4) {
+		this.sfzh_jtcy4 = sfzh_jtcy4;
+	}
+	public String getRel4() {
+		return rel4;
+	}
+	public void setRel4(String rel4) {
+		this.rel4 = rel4;
+	}
+	public String getRes4() {
+		return res4;
+	}
+	public void setRes4(String res4) {
+		this.res4 = res4;
+	}
+	public String getBody4() {
+		return body4;
+	}
+	public void setBody4(String body4) {
+		this.body4 = body4;
+	}
+	public String getQx() {
+		return qx;
+	}
+	public void setQx(String qx) {
+		this.qx = qx;
+	}
+	public String getJd() {
+		return jd;
+	}
+	public void setJd(String jd) {
+		this.jd = jd;
+	}
+	public String getSq() {
+		return sq;
+	}
+	public void setSq(String sq) {
+		this.sq = sq;
+	}
+	
+	public String getApproveresult1() {
+		return approveresult1;
+	}
+	public void setApproveresult1(String approveresult1) {
+		this.approveresult1 = approveresult1;
+	}
+	public String getApproveperson1() {
+		return approveperson1;
+	}
+	public void setApproveperson1(String approveperson1) {
+		this.approveperson1 = approveperson1;
+	}
+	public String getApproveidea1() {
+		return approveidea1;
+	}
+	public void setApproveidea1(String approveidea1) {
+		this.approveidea1 = approveidea1;
+	}
+	public String getAprrovedate1() {
+		return aprrovedate1;
+	}
+	public void setAprrovedate1(String aprrovedate1) {
+		this.aprrovedate1 = aprrovedate1;
+	}
+	public String getApproveresult2() {
+		return approveresult2;
+	}
+	public void setApproveresult2(String approveresult2) {
+		this.approveresult2 = approveresult2;
+	}
+	public String getApproveperson2() {
+		return approveperson2;
+	}
+	public void setApproveperson2(String approveperson2) {
+		this.approveperson2 = approveperson2;
+	}
+	public String getApproveidea2() {
+		return approveidea2;
+	}
+	public void setApproveidea2(String approveidea2) {
+		this.approveidea2 = approveidea2;
+	}
+	public String getAprrovedate2() {
+		return aprrovedate2;
+	}
+	public void setAprrovedate2(String aprrovedate2) {
+		this.aprrovedate2 = aprrovedate2;
+	}
+	public String getApproveresult3() {
+		return approveresult3;
+	}
+	public void setApproveresult3(String approveresult3) {
+		this.approveresult3 = approveresult3;
+	}
+	public String getApproveperson3() {
+		return approveperson3;
+	}
+	public void setApproveperson3(String approveperson3) {
+		this.approveperson3 = approveperson3;
+	}
+	public String getApproveidea3() {
+		return approveidea3;
+	}
+	public void setApproveidea3(String approveidea3) {
+		this.approveidea3 = approveidea3;
+	}
+	public String getAprrovedate3() {
+		return aprrovedate3;
+	}
+	public void setAprrovedate3(String aprrovedate3) {
+		this.aprrovedate3 = aprrovedate3;
+	}
+	public String getApprovegoto() {
+		return approvegoto;
+	}
+	public void setApprovegoto(String approvegoto) {
+		this.approvegoto = approvegoto;
+	}
+	public String getSerialno() {
+		return serialno;
+	}
+	public void setSerialno(String serialno) {
+		this.serialno = serialno;
+	}
+	public String getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(String createtime) {
+		this.createtime = createtime;
+	}
+	public String getApproveend() {
+		return approveend;
+	}
+	public void setApproveend(String approveend) {
+		this.approveend = approveend;
+	}
+	public String getUpdatetime() {
+		return updatetime;
+	}
+	public void setUpdatetime(String updatetime) {
+		this.updatetime = updatetime;
+	}
+	public String getApprovemoney() {
+		return approvemoney;
+	}
+	public void setApprovemoney(String approvemoney) {
+		this.approvemoney = approvemoney;
+	}
+	public String getTjzId() {
+		return tjzId;
+	}
+	public void setTjzId(String tjzId) {
+		this.tjzId = tjzId;
+	}
+	
+	
+}
